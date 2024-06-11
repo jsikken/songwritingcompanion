4ba5c95a4118c0000303a3ef6ac79988d829d448
jsikken <130744129+jsikken@users.noreply.github.com>
2024-06-11 11:35:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded', function() {
         "B maj": ["B1", "C#2", "D#2", "E2", "F#2", "G#2", "A#2", "B2"],
         "F# maj": ["F#1", "G#1", "A#1", "B1", "C#2", "D#2", "E#2", "F#2"],
         "C# maj": ["C#1", "D#1", "E#1", "F#1", "G#1", "A#1", "B#1", "C#2"],
-        "Ab maj": ["Ab1", "Bb1", "C2", "Db2", "Eb2", "F2", "G2", "Ab2"],
-        "Eb maj": ["Eb1", "F1", "G1", "Ab1", "Bb1", "C2", "D2", "Eb2"],
-        "Bb maj": ["Bb1", "C2", "D2", "Eb2", "F2", "G2", "A2", "Bb2"],
-        "F maj": ["F1", "G1", "A1", "Bb1", "C2", "D2", "E2", "F2"]
+        "Ab maj": ["G#1", "A#1", "C2", "C#2", "D#2", "F2", "G2", "G#2"],
+        "Eb maj": ["D#1", "F1", "G1", "G#1", "A#1", "C2", "D2", "D#2"],
+        "Bb maj": ["A#1", "C2", "D2", "D#2", "F2", "G2", "A2", "A#2"],
+        "F maj": ["F1", "G1", "A1", "A#1", "C2", "D2", "E2", "F2"]
     };
     const minorNotes = {
         "A min": ["A1", "B1", "C2", "D2", "E2", "F2", "G2", "A2"],
@@ -24,10 +24,10 @@ document.addEventListener('DOMContentLoaded', function() {
         "G# min": ["G#1", "A#1", "B1", "C#2", "D#2", "E2", "F#2", "G#2"],
         "D# min": ["D#1", "E#1", "F#1", "G#1", "A#1", "B1", "C#2", "D#2"],
         "A# min": ["A#1", "B#1", "C#2", "D#2", "E#2", "F#2", "G#2", "A#2"],
-        "F min": ["F1", "G1", "Ab1", "Bb1", "C2", "Db2", "Eb2", "F2"],
-        "C min": ["C1", "D1", "Eb1", "F1", "G1", "Ab1", "Bb1", "C2"],
-        "G min": ["G1", "A1", "Bb1", "C2", "D2", "Eb2", "F2", "G2"],
-        "D min": ["D1", "E1", "F1", "G1", "A1", "Bb1", "C2", "D2"]
+        "F min": ["F1", "G1", "G#1", "A#1", "C2", "C#2", "D#2", "F2"],
+        "C min": ["C1", "D1", "D#1", "F1", "G1", "G#1", "A#1", "C2"],
+        "G min": ["G1", "A1", "A#1", "C2", "D2", "D#2", "F2", "G2"],
+        "D min": ["D1", "E1", "F1", "G1", "A1", "A#1", "C2", "D2"]
     };
 
     const majorBox = document.getElementById('majorBox');
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
             key.classList.add('piano-key');
             if (blackKeyOffsets.includes(i % 12)) {
                 key.classList.add('black-key');
-                key.style.left = `${(i - Math.floor(i / 12)) * 20 - 6}px`;
+                key.style.left = `${(i % 12) * 20 - 6}px`;
             } else {
                 key.classList.add('white-key');
             }
